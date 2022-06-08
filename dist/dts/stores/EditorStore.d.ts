14b59507c5456edfa19c981b1b62f518a492d510
jaewoong_ryu <jaewoong_ryu@tmax.co.kr>
2022-06-08 19:57:02 +0900

    chore: build commit

--- dist/dts/stores/EditorStore.d.ts
+++ dist/dts/stores/EditorStore.d.ts
@@ -1,6 +1,19 @@
 export declare class EditorStore {
     tinymce: any;
     rootStore: any;
+    marker: any;
+    searchInputValue: any;
+    searchingKeyword: string;
+    searchTotalCount: number;
+    searchCurrentIndex: any;
+    searchedEleArr: any;
     constructor(rootStore: any);
     setEditor(editorInstance: any): void;
+    setMarker(el: HTMLElement): void;
+    mark(keyword: string): void;
+    unmark(): void;
+    setSearchInputValue(value: string): void;
+    endSearching(): void;
+    handleSearchEditor(): void;
+    handleSearchNext(): void;
 }
