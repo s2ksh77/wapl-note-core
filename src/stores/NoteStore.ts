1f89d6b6563c7a1d46fdbb605da13b27b96c3e7b
jaewoong_ryu <jaewoong_ryu@tmax.co.kr>
2022-06-21 14:00:51 +0900

    feat: 검색결과 하이라이팅을 위한 로직 추가

--- src/stores/NoteStore.ts
+++ src/stores/NoteStore.ts
@@ -3,11 +3,14 @@ import { makeAutoObservable } from 'mobx';
 import type { ChannelId } from '~/@types/common';
 import { ISearchRepo } from '~/repositories/SearchRepoType';
 import { SearchRepoImpl } from '~/repositories';
+import Mark from 'mark.js';
 
 export class NoteStore {
   rootStore;
   searchRepo: ISearchRepo;
   headerTitle = '';
+  marker;
+  searchKeyword = '';
 
   constructor(rootStore) {
     makeAutoObservable(this);
@@ -21,5 +24,30 @@ export class NoteStore {
   ): Promise<DTO.SearchResponse> {
     const res = await this.searchRepo.getSearchList(searchKey, channelId); // no model
     return res;
+  }
+
+  setMarker(el: HTMLElement): void {
+    this.marker = new Mark(el);
+  }
+
+  mark(): void {
+    this.marker.mark(this.searchKeyword, {
+      accuracy: {
+        value: 'partially',
+        limiters: [],
+      },
+    });
+  }
+
+  unmark(option): void {
+    this.marker.unmark(option);
+  }
+
+  setSearchKeyword(keyword: string): void {
+    this.searchKeyword = keyword;
+  }
+
+  endSearching(): void {
+    this.searchKeyword = '';
   }
 }
